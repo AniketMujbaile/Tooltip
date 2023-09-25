a707f7e1b87b9804d5e69b2cadf163d180dce6d5
Aniket Mujbaile <aniketmujbaile1@gmail.com>
2023-09-25 17:22:29 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Tooltip from './Components/Tooltip';
+
+function App() {
+  return (
+    <div className="App">
+    <Tooltip/>
+    </div>
+  );
+}
+
+export default App;
